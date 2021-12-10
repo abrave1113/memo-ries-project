53275c1a832af6caba3e140b94a2fba7698d98ed
abrave1113 <abrave1113@gmail.com>
2021-12-10 18:38:20 -0500

    mc

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.use(express.static("client/build"));
 
 router.get('*', (req, res) => {
-  res.sendFile('client/build', 'index.html');
+  res.sendFile('./client/build', 'index.html');
 });
 
 router.options('/', (req, res, next) => {
