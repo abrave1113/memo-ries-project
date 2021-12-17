a163f50b0f7f66f29ef8ff4abc63548334dfc370
abrave1113 <abrave1113@gmail.com>
2021-12-17 16:00:49 -0500

    mc

--- server/client/src/actions/auth.js
+++ server/client/src/actions/auth.js
@@ -3,7 +3,7 @@ import * as api from '../api/index.js';
 
 export const signin = (formData, history) => async (dispatch) => {
     try {
-        const { data } = await axios.signIn(formData);
+        const { data } = await api.signIn(formData);
 
         dispatch({ type: AUTH, data });
 
@@ -15,7 +15,7 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
     try {
-        const { data } = await axios.signUp(formData);
+        const { data } = await api.signUp(formData);
         
         dispatch({ type: AUTH, data });
         
--- server/client/src/actions/posts.js
+++ server/client/src/actions/posts.js
@@ -4,7 +4,7 @@ import * as api from '../api/index.js';
 // Action Creators
 export const getPosts = () => async (dispatch) => {
     try {
-        const { data } = await axios.fetchPosts();
+        const { data } = await api.fetchPosts();
 
         dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
@@ -14,7 +14,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await axios.createPost(post);
+        const { data } = await api.createPost(post);
 
         dispatch({ type: CREATE, payload: data })
     } catch (error) {
@@ -24,7 +24,7 @@ export const createPost = (post) => async (dispatch) => {
 
 export const updatePost = (id, post) => async (dispatch) => {
     try {
-        const { data } = await axios.updatePost(id, post);
+        const { data } = await api.updatePost(id, post);
 
         dispatch({ type: UPDATE, payload: data });
     } catch (error) {
@@ -34,7 +34,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
     try {
-        await axios.deletePost(id);
+        await api.deletePost(id);
 
         dispatch({type: DELETE, payload: id});
     } catch (error) {
@@ -44,7 +44,7 @@ export const deletePost = (id) => async (dispatch) => {
 
 export const likePost = (id) => async (dispatch) => {
     try {
-        const { data } = await axios.likePost(id);
+        const { data } = await api.likePost(id);
 
         dispatch({ type: UPDATE, payload: data });
     } catch (error) {
