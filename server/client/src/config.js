49a2c777dd94c998ae04e8a7d9ca50b26e182edc
abrave1113 <abrave1113@gmail.com>
2021-12-17 14:12:56 -0500

    rm ai

--- server/client/src/config.js
+++ server/client/src/config.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+// import axios from "axios"
 
-export const axiosInstance = axios.create({
-    baseURL: "https://memo-ries-project.herokuapp.com/api"
-})
+// export const axiosInstance = axios.create({
+//     baseURL: "https://memo-ries-project.herokuapp.com/api"
+// })
